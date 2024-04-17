f135d7379fff6b77c2c6032ec055cd1266a27d97
Mark Dayton <asrivatsa6@gmail.com>
2024-04-17 10:59:54 +0530

    add null safety

--- db/query/weatherTelemetry.sql
+++ db/query/weatherTelemetry.sql
@@ -17,12 +17,18 @@ WHERE millis > $1
 ORDER BY millis ASC
 LIMIT $2;
 -- name: GetMeanWeatherTelemetry :one
-SELECT AVG(temperature) AS temperature,
-    AVG(humidity) AS humidity,
-    AVG(windSpeed) AS windSpeed,
-    AVG(windDirection) AS windDirection,
-    AVG(pressure) AS pressure,
-    AVG(waterAmount) AS waterAmount
+SELECT CAST(
+        COALESCE(AVG(temperature), 0) AS DOUBLE PRECISION
+    ) AS temperature,
+    CAST(COALESCE(AVG(humidity), 0) AS DOUBLE PRECISION) AS humidity,
+    CAST(COALESCE(AVG(windSpeed), 0) AS DOUBLE PRECISION) AS windSpeed,
+    CAST(
+        COALESCE(AVG(windDirection), 0) AS DOUBLE PRECISION
+    ) AS windDirection,
+    CAST(COALESCE(AVG(pressure), 0) AS DOUBLE PRECISION) AS pressure,
+    CAST(
+        COALESCE(AVG(waterAmount), 0) AS DOUBLE PRECISION
+    ) AS waterAmount
 FROM weatherTelemetry
 WHERE millis >= $1
     and millis < $2
